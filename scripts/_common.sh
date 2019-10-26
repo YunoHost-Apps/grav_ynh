3bfe16f230ad5b1b3e15eb7e78f7a31b31aba4f3
tituspijean <tituspijean@outlook.com>
2019-10-26 19:02:22 +0200

    [upg] grav 1.6.16 Makes the app agnostic regarding the needed PHP version. Includes experimental helpers to install various PHP versions, if needed. Includes Grav v1.6.16 with PHP 7.2.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="php7.2-zip php7.2-mbstring php7.2-curl php7.2-dom php7.2-gd php7.2-xml php7.2-ldap"
+
+# Version numbers
+phpversion="7.2"
+
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
 # Execute a command as another user
 # usage: exec_as USER COMMAND [ARG ...]
 exec_as() {
