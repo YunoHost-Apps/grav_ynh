aad6b2e2efa45f42b90e022db382e26a7c535d1b
ericgaspar <junk.eg@free.fr>
2021-11-29 20:25:33 +0100

    4.3

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,22 +8,8 @@
 YNH_PHP_VERSION="7.3"
 
 # dependencies used by the app
-extra_php_dependencies="php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-ldap"
+pkg_dependencies="php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-ldap"
 
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
-
-# Execute a command as another user
-# usage: exec_as USER COMMAND [ARG ...]
-exec_as() {
-	local USER=$1
-	shift 1
-
-	if [[ $USER = $(whoami) ]]
-	then
-		eval $@
-	else
-		sudo -u "$USER" $@
-	fi
-}
