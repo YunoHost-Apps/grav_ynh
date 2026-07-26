bd7df33f59a9aead52a76c6fa461a2462d3c1ae1
tituspijean <tituspijean@yunohost.org>
2026-07-26 09:24:12 +0200

    fix(scripts): indent

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,10 +15,10 @@ ynh_system_user_add_group() {
     ynh_handle_getopts_args "$@"
     groups="${groups:-}"
 
-	local group
-	for group in $groups; do
-		usermod -a -G "$group" "$username"
-	done
+    local group
+    for group in $groups; do
+        usermod -a -G "$group" "$username"
+    done
 }
 
 
@@ -33,8 +33,8 @@ ynh_system_user_del_group() {
     ynh_handle_getopts_args "$@"
     groups="${groups:-}"
 
-	local group
-	for group in $groups; do
-		gpasswd -d "$username" "$group"
-	done
+    local group
+    for group in $groups; do
+        gpasswd -d "$username" "$group"
+    done
 }
